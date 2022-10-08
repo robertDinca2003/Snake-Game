dbb553ddf26f80da96e1cb427c4e59b6ec81073b
Robert Dinca <71851178+robertDinca2003@users.noreply.github.com>
2022-10-08 12:34:42 +0300

    Add files via upload

---
+++ SnakeSarpe/SnakeSarpe/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SnakeSarpe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSnake = new System.Windows.Forms.Label();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.lblBestScore = new System.Windows.Forms.Label();
+            this.panelGame = new System.Windows.Forms.Panel();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.lblCurentScore = new System.Windows.Forms.Label();
+            this.timerClock = new System.Windows.Forms.Timer(this.components);
+            this.panelGame.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSnake
+            // 
+            this.lblSnake.AutoSize = true;
+            this.lblSnake.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSnake.ForeColor = System.Drawing.Color.Gold;
+            this.lblSnake.Location = new System.Drawing.Point(102, 32);
+            this.lblSnake.Name = "lblSnake";
+            this.lblSnake.Size = new System.Drawing.Size(477, 163);
+            this.lblSnake.TabIndex = 0;
+            this.lblSnake.Text = "Snake";
+            // 
+            // btn_play
+            // 
+            this.btn_play.BackColor = System.Drawing.Color.Gold;
+            this.btn_play.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_play.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btn_play.Location = new System.Drawing.Point(162, 377);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(328, 119);
+            this.btn_play.TabIndex = 1;
+            this.btn_play.Text = "PLAY";
+            this.btn_play.UseVisualStyleBackColor = false;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.Gold;
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btn_exit.Location = new System.Drawing.Point(162, 566);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(328, 119);
+            this.btn_exit.TabIndex = 2;
+            this.btn_exit.Text = "EXIT";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblBestScore
+            // 
+            this.lblBestScore.AutoSize = true;
+            this.lblBestScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBestScore.ForeColor = System.Drawing.Color.Gold;
+            this.lblBestScore.Location = new System.Drawing.Point(93, 879);
+            this.lblBestScore.Name = "lblBestScore";
+            this.lblBestScore.Size = new System.Drawing.Size(248, 52);
+            this.lblBestScore.TabIndex = 3;
+            this.lblBestScore.Text = "Best Score:";
+            // 
+            // panelGame
+            // 
+            this.panelGame.Controls.Add(this.btn_pause);
+            this.panelGame.Controls.Add(this.btn_back);
+            this.panelGame.Controls.Add(this.lblCurentScore);
+            this.panelGame.Controls.Add(this.lblBestScore);
+            this.panelGame.Controls.Add(this.btn_exit);
+            this.panelGame.Controls.Add(this.btn_play);
+            this.panelGame.Controls.Add(this.lblSnake);
+            this.panelGame.Location = new System.Drawing.Point(1, 4);
+            this.panelGame.Name = "panelGame";
+            this.panelGame.Size = new System.Drawing.Size(699, 1022);
+            this.panelGame.TabIndex = 4;
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.BackColor = System.Drawing.Color.Gold;
+            this.btn_pause.Enabled = false;
+            this.btn_pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_pause.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btn_pause.Location = new System.Drawing.Point(567, 33);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(80, 84);
+            this.btn_pause.TabIndex = 6;
+            this.btn_pause.Text = "||";
+            this.btn_pause.UseVisualStyleBackColor = false;
+            this.btn_pause.Visible = false;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_back
+            // 
+            this.btn_back.BackColor = System.Drawing.Color.Gold;
+            this.btn_back.Enabled = false;
+            this.btn_back.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_back.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btn_back.Location = new System.Drawing.Point(470, 892);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(195, 69);
+            this.btn_back.TabIndex = 5;
+            this.btn_back.Text = "Back";
+            this.btn_back.UseVisualStyleBackColor = false;
+            this.btn_back.Visible = false;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // lblCurentScore
+            // 
+            this.lblCurentScore.AutoSize = true;
+            this.lblCurentScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurentScore.ForeColor = System.Drawing.Color.Gold;
+            this.lblCurentScore.Location = new System.Drawing.Point(21, 931);
+            this.lblCurentScore.Name = "lblCurentScore";
+            this.lblCurentScore.Size = new System.Drawing.Size(307, 52);
+            this.lblCurentScore.TabIndex = 4;
+            this.lblCurentScore.Text = "Current Score:";
+            this.lblCurentScore.Visible = false;
+            // 
+            // timerClock
+            // 
+            this.timerClock.Interval = 400;
+            this.timerClock.Tick += new System.EventHandler(this.timerClock_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(698, 1024);
+            this.Controls.Add(this.panelGame);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(720, 1080);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(720, 1080);
+            this.Name = "Form1";
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panelGame.ResumeLayout(false);
+            this.panelGame.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSnake;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label lblBestScore;
+        private System.Windows.Forms.Panel panelGame;
+        private System.Windows.Forms.Timer timerClock;
+        private System.Windows.Forms.Label lblCurentScore;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_back;
+    }
+}
+
---
+++ SnakeSarpe/SnakeSarpe/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeSarpe
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<Point, bool> ht = new Dictionary<Point, bool>();
+        Point[] snake = new Point[257];
+        Random rand = new Random();
+        Graphics g;
+        int[,] dir = { {-1,0},{1,0 },{0,-1 },{0,1 } };
+        Button[] btns = new Button[4];
+        int currentDir ;
+        int currentScore;
+        int bestScore = 0;
+        Point pnt = new Point(-1,-1);
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        void winner()
+        {
+            timerClock.Enabled = false;
+            lblSnake.Text = "Won!";
+
+            btn_back.Visible = true;
+            btn_back.Enabled = true;
+            if (currentScore > bestScore) bestScore = currentScore;
+            lblBestScore.Text = "Best Score: " + bestScore.ToString();
+        }
+
+        void loser()
+        {
+            lblSnake.Text = "Lose!";
+            timerClock.Enabled = false;
+
+            btn_back.Visible = true;
+            btn_back.Enabled = true;
+
+            if (currentScore > bestScore) bestScore = currentScore;
+            lblBestScore.Text = "Best Score: " + bestScore.ToString();
+        }
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            btn_play.Enabled = false;
+            btn_play.Visible = false;
+            btn_exit.Enabled = false;
+            btn_exit.Visible = false;
+
+            lblBestScore.Visible = false;
+
+            lblCurentScore.Visible = true;
+
+            btn_pause.Visible = true;
+            btn_pause.Enabled = true;
+
+            ht.Clear();
+            for (int i = 0; i <= currentScore; i++) snake[i] = new Point(0,0);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Button temp = new Button();
+                panelGame.Controls.Add(temp);
+                temp.Height = 50;
+                temp.Width = 50;
+                temp.BackColor = Color.Gold;
+                temp.Font = new Font("Arial", 20, FontStyle.Bold);
+                temp.ForeColor = Color.SteelBlue;
+                temp.Location = new Point(205 + dir[i, 0] * 190, 335 + dir[i, 1] * 190);
+                temp.Click += new EventHandler(this.btn_dir);
+                btns[i] = temp;
+            }
+            btns[0].Text = "<";
+            btns[1].Text = ">";
+            btns[2].Text = "^";
+            btns[3].Text = "v";
+
+
+            g = panelGame.CreateGraphics();
+
+            for (int i = 0; i < 16; i++)
+                for (int j = 0; j < 16; j++)
+                    g.FillRectangle(new SolidBrush(Color.LightGreen),70+i*20,200+j*20,20,20);
+
+            currentDir = 3;
+            currentScore = 0;
+            lblCurentScore.Text = "Current Score: " + currentScore.ToString(); 
+            timerClock.Enabled = true;
+        
+            snake[0] =(new Point(8, 8));
+            pnt = snake[0];
+            ht.Add(snake[0], true);
+            while (pnt == snake[0] )
+            { pnt.X = rand.Next(0,15); pnt.Y = rand.Next(0,15); }
+            g.FillRectangle(new SolidBrush(Color.Green), 70 + pnt.X * 20, 200 + pnt.Y * 20, 20, 20);
+
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            if (currentScore == 256) winner();
+            int x = (snake[0].X + dir[currentDir, 0]) % 16;
+            if (x < 0) x = 15;
+            int y = (snake[0].Y + dir[currentDir, 1])%16;
+            if (y < 0) y = 15;
+            Point temp = new Point(x,y) ;
+            Point temp2= new Point(-1,1);
+
+            
+
+            if (ht.ContainsKey(temp) == true) { loser();return; }
+
+            g.FillRectangle(new SolidBrush(Color.Red), 70 + temp.X * 20, 200 + temp.Y * 20, 20, 20);
+
+            for(int i = 0; i <= currentScore; i++)
+            {
+                temp2 = snake[i];
+                snake[i] = temp;
+                temp = temp2;
+            }
+            //MessageBox.Show(temp2.X.ToString() + " " + temp2.Y.ToString());
+            if(temp2 == pnt)
+            {
+                currentScore++;
+                snake[currentScore] = temp;
+                lblCurentScore.Text = "Current Score: " + currentScore.ToString();
+
+                while (ht.ContainsKey(pnt) == true) {pnt.X = rand.Next(0, 15); pnt.Y = rand.Next(0, 15); }
+                g.FillRectangle(new SolidBrush(Color.Green), 70 + pnt.X * 20, 200 + pnt.Y * 20, 20, 20);
+
+            }
+            else
+                g.FillRectangle(new SolidBrush(Color.LightGreen), 70 + temp.X * 20, 200 + temp.Y * 20, 20, 20);
+            ht.Clear();
+            for (int i = 0; i <= currentScore; i++) ht.Add(snake[i], true);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {//MessageBox.Show(e.KeyCode.ToString());
+            
+        }
+
+        private void btn_dir(object sender, EventArgs e)
+        {
+            if (sender.Equals(btns[0]) && currentDir != 1) currentDir = 0;
+            if (sender.Equals(btns[1]) && currentDir != 0) currentDir = 1;
+            if (sender.Equals(btns[2]) && currentDir != 3) currentDir = 2;
+            if (sender.Equals(btns[3]) && currentDir != 2) currentDir = 3;
+
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            if (lblSnake.Text != "Snake") return;
+            if(timerClock.Enabled == true)
+            timerClock.Enabled = false;
+            else 
+            timerClock.Enabled = true;
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.SteelBlue);
+            for (int i = 0; i < 4; i++) panelGame.Controls.Remove(btns[i]);
+            lblSnake.Text = "Snake";
+            lblCurentScore.Visible = false;
+            btn_back.Enabled = false;
+            btn_back.Visible = false;
+
+            btn_play.Visible = true;
+            btn_play.Enabled = true;
+
+            btn_exit.Enabled = true;
+            btn_exit.Visible = true;
+
+            lblBestScore.Visible = true;
+
+        }
+    }
+}
